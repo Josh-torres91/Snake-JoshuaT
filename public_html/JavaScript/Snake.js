37242578803ed1859fa9dff93e45a9d14f95856d
Joshua Torres <mjolnir_mkv@yahoo.com>
2014-10-06 10:44:32 -0700

    New Additions
    These are new additions to the coding of the snake UI.

--- public_html/JavaScript/Snake.js
+++ public_html/JavaScript/Snake.js
@@ -1,2 +1,25 @@
+var snake; 
 
+var context; 
+var sreenwidth;
+var screenheight; 
 
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getCOntext("2d");
+    
+     screenwidth = window.innerWidth;
+     screenheight = window.innerHeight; 
+     
+     canvas.width = sreenWidth;
+     canvas.Height = screenHeight; 
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillstyle = "rgb(5, 227, 64)";
+    context.fillrect 
+}
