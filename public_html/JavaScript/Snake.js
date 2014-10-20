90da394789aaa211a4499ff44eb71e3cb27bd955
Joshua Torres <mjolnir_mkv@yahoo.com>
2014-10-20 10:45:05 -0700

    New Additions
    I have added new additions to the js file code.

--- public_html/JavaScript/Snake.js
+++ public_html/JavaScript/Snake.js
@@ -84,10 +84,10 @@ function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-     if(snakeDirection === "down") {
+     if(snakeDirection == "down") {
          snakeHeadY++;
      }
-     else {
+     else if(snakeDirection == "right"){
          snakeHeadX++;
      }
      
@@ -123,6 +123,26 @@ function setFoodPosition() {
     food.x = randomX;
     food.y = randomY;
 }
+/*-----------------------------------------------------------------------------
+ * Input Functions
+ * ----------------------------------------------------------------------------
+ */
 function keyboardHandler(event) {
     console.log(event);
+    
+    if(event.keyCode == "39") {
+        snakeDirection = "right";
+    }
+    else if(event.keyCode == "40") {
+        snakeDirection = "down"; 
+    }
+
+}
+
+function keyboardHandler(event) {
+    console.log(event);
+    
+    if(event.keyCode == "") {
+        snake
+    }
 }
